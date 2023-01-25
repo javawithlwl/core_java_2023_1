06349f0d4c53e085b4eff63ce839b7d45356eb07
javawithlwl <javawithlwl@gmail.com>
2023-01-25 08:53:07 +0530

    Added array examples

--- src/main/java/com/careerit/jfs/basics/day7/ArrayExample.java
+++ src/main/java/com/careerit/jfs/basics/day7/ArrayExample.java
@@ -1,15 +1,24 @@
 package com.careerit.jfs.basics.day7;
 
+import java.util.Arrays;
+
 public class ArrayExample {
 
   public static void main(String[] args) {
 
-    int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int sum = 0;
-    for (int ele : arr) {
-      sum += ele;
-    }
-    System.out.println("The sum of array element is :" + sum);
+    int[] arr1 = new int[]{1, 2, 3, 14, 15, 16};
+    int[] arr2 = new int[]{11, 12, 13, 4, 5, 6};
+
+    System.out.println(Arrays.toString(arr1));
+    System.out.println(Arrays.toString(arr2));
+
+    int[] arr3 = new int[arr1.length+arr2.length];
+
+    System.arraycopy(arr1,0,arr3,0,arr1.length);
+    System.arraycopy(arr2,0,arr3,arr1.length,arr2.length);
+    Arrays.sort(arr3);
+    System.out.println(Arrays.toString(arr3));
+
 
   }
 
