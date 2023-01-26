b9c091235d408ff6cfe93cf1b102d6d4c9edecbb
javawithlwl <javawithlwl@gmail.com>
2023-01-26 09:05:19 +0530

    Added array examples

---
+++ src/main/java/com/careerit/jfs/basics/day8/ArrayExample.java
@@ -0,0 +1,10 @@
+package com.careerit.jfs.basics.day8;
+
+public class ArrayExample {
+  public static void main(String[] args) {
+        String[] arr = new String[]{"Krish","Manoj","Chran","Kiran","Jayesh","Suresh"};
+        for(String name:arr){
+          System.out.println(name);
+        }
+  }
+}
