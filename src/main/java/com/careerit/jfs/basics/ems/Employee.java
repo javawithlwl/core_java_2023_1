47891dd44e44a3ab1bbe0ca725ec64fc017c94bf
javawithlwl <javawithlwl@gmail.com>
2023-02-04 09:15:34 +0530

    Added ems module and create bean object

---
+++ src/main/java/com/careerit/jfs/basics/ems/Employee.java
@@ -0,0 +1,13 @@
+package com.careerit.jfs.basics.ems;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Employee {
+
+  private String empno;
+  private String ename;
+  private double salary;
+}
